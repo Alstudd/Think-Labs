a1c0fceb1c73ec7e0893b6aca13f579d038e8ad1
Alstudd <alstonsoares17@gmail.com>
2024-04-14 05:57:18 +0530

    nav tool

--- devopia-paypal-mafia/src/app/course/[id]/page.tsx
+++ devopia-paypal-mafia/src/app/course/[id]/page.tsx
@@ -6,83 +6,97 @@ import QuizCards from "@/components/QuizCards";
 import axios from "axios";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 import React, { useState, useEffect } from "react";
 
 export default function CoursePage({ params }) {
-	const [data, setData] = useState();
-	const [currPage, setcurrPage] = useState(0);
-	const id = params.id;
-	console.log(id);
+  const [data, setData] = useState([]);
+  const [currUnit, setCurrUnit] = useState(0);
+  const [allUnits, setAllUnits] = useState([]);
+  const id = params.id;
+  console.log(id);
 
-	useEffect(() => {
-		const getData = async () => {
-			const res = await axios.post("/api/getCourses", { courseId: id });
-			console.log(res.data);
-			setData(res.data);
-		};
-		getData();
-	}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.post("/api/getCourses", { courseId: id });
+        setData(res.data);
+        let units = [];
+        res.data.forEach((element) => {
+          units = units.concat(element.children);
+        });
+        setAllUnits(units);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchData();
+  }, [id]);
 
-	return (
-		<div>
-			{/* <Navbar /> */}
-			<CourseSideBar data={data} />
-			<div>
-				<div className="ml-[400px] px-8">
-					<div className="flex">
-						<MainVideoSummary
-							videoId={data[0].children[0].videoId}
-						/>
-						{/* <QuizCards /> */}
-					</div>
+  const changeCurrUnit = (unitId) => {
+    const index = allUnits.findIndex((unit) => unit.id === unitId);
+    if (index !== -1) {
+      setCurrUnit(index);
+    }
+  };
 
-					<div className="flex-[1] h-[1px] mt-4 text-gray-500 bg-gray-500" />
-					<div className="flex pb-8">
-						{/* {prevChapter && ( */}
-						<Link
-							// href={`/course/${course.id}/${chapterIndex}/${videoIndex - 1}`}
-							href={"#"}
-							className="flex mt-4 mr-auto w-fit"
-						>
-							<div className="flex items-center">
-								<ChevronLeft className="w-6 h-6 mr-1" />
-								<div className="flex flex-col items-start">
-									<span className="text-sm text-secondary-foreground/60">
-										Previous
-									</span>
-									<span className="text-xl font-bold">
-										{/* {prevChapter.name} */}
-										Prev Name
-									</span>
-								</div>
-							</div>
-						</Link>
-						{/* )} */}
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <CourseSideBar
+        data={data}
+        currentChapterId={allUnits[currUnit]?.id}
+        changeCurrUnit={changeCurrUnit}
+      />
+      <div>
+        <div className="ml-[400px] px-8">
+          <div className="flex">
+            {data.length > 0 && allUnits.length > 0 && (
+              <MainVideoSummary video={allUnits[currUnit]?.videoId} />
+            )}
+          </div>
 
-						{/* {nextChapter && ( */}
-						<Link
-							// href={`/course/${course.id}/${chapterIndex}/${videoIndex + 1}`}
-							href={"#"}
-							className="flex mt-4 ml-auto w-fit"
-						>
-							<div className="flex items-center">
-								<div className="flex flex-col items-start">
-									<span className="text-sm text-secondary-foreground/60">
-										Next
-									</span>
-									<span className="text-xl font-bold">
-										{/* {nextChapter.name} */}
-										Next Name
-									</span>
-								</div>
-								<ChevronRight className="w-6 h-6 ml-1" />
-							</div>
-						</Link>
-						{/* )} */}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="flex-[1] h-[1px] mt-4 text-gray-500 bg-gray-500" />
+          <div className="flex pb-8">
+            <div
+              onClick={() =>
+                setCurrUnit((currUnit - 1 + allUnits.length) % allUnits.length)
+              }
+              className="flex mt-4 mr-auto w-fit cursor-pointer"
+            >
+              <div className="flex items-center">
+                <ChevronLeft className="w-6 h-6 mr-1" />
+                <div className="flex flex-col items-start">
+                  <span className="text-sm text-secondary-foreground/60">
+                    Previous
+                  </span>
+                  <span className="text-xl font-bold">
+                    {allUnits[(currUnit - 1 + allUnits.length) % allUnits.length]?.name}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div
+              onClick={() =>
+                setCurrUnit((currUnit + 1) % allUnits.length)
+              }
+              className="flex mt-4 ml-auto w-fit cursor-pointer"
+            >
+              <div className="flex items-center">
+                <div className="flex flex-col items-start">
+                  <span className="text-sm text-secondary-foreground/60">
+                    Next
+                  </span>
+                  <span className="text-xl font-bold">
+                    {allUnits[(currUnit + 1) % allUnits.length]?.name}
+                  </span>
+                </div>
+                <ChevronRight className="w-6 h-6 ml-1" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
